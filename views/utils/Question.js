c1668c03388ead9481d8143751c8647db28d5972
Utsav Joshi <joshiutsav56@gmail.com>
2023-08-26 18:04:03 +0530

    23|08|2023

--- views/utils/Question.js
+++ views/utils/Question.js
@@ -1,36 +1,549 @@
 var question = [
     {
-        Question: "What is a SQL full form?",
-        A: "Structured query language",
-        B: "Query language",
-        C: "Structured language",
-        D: "None of them",
-        Answer: "a"
+        Question: "Physical part of a computer are known as___________",
+        A: "Software",
+        B: "Hardware",
+        C: "Operating System",
+        D: "System unit",
+        Answer: "B"
     },
     {
-        Question: "What is another question?",
-        A: "Option A",
-        B: "Option B",
-        C: "Option C",
-        D: "Option D",
-        Answer: "a"
+        Question: "___________is a place where processing takes place",
+        A: "Box",
+        B: "CPU",
+        C: "Monitor",
+        D: "System unit",
+        Answer: "B"
     },
     {
-        Question: "What is another question?",
-        A: "Option E",
-        B: "Option B",
-        C: "Option C",
-        D: "Option D",
-        Answer: "a"
+        Question: "Processor is fixed on the",
+        A: "Fan",
+        B: "Chipboard",
+        C: "Motherboard",
+        D: "Expansion slot",
+        Answer: "C"
     },
     {
-        Question: "What is another question?",
-        A: "Option A",
-        B: "Option B",
-        C: "Option C",
-        D: "Option D",
-        Answer: "a"
+        Question: "There are         types of memory",
+        A: "Two",
+        B: "Three",
+        C: "One",
+        D: "Four",
+        Answer: "A"
     },
-]
+    {
+        Question: "A Processor acts like a/an",
+        A: "Heart",
+        B: "Arm",
+        C: "Brain",
+        D: "Kidney",
+        Answer: "C"
+    },
+    {
+        Question: "Arithmetic operations are performed by",
+        A: "LU",
+        B: "AU",
+        C: "ALU",
+        D: "CU",
+        Answer: "C"
+    },
+    {
+        Question: "A Processor is also known as",
+        A: "CPU",
+        B: "UPS",
+        C: "UPC",
+        D: "PUS",
+        Answer: "A"
+    },
+    {
+        Question: "Which of the following part of the CPU controls the sequence of the instructions?",
+        A: "AU",
+        B: "CU",
+        C: "LU",
+        D: "ALU",
+        Answer: "B"
+    },
+    {
+        Question: "RAM stands for",
+        A: "Random Access Memory",
+        B: "Random Analysis",
+        C: "Read Able Memory",
+        D: "Random Available Memory",
+        Answer: "A"
+    },
+    {
+        Question: "ROM stores data",
+        A: "Permanently",
+        B: "Temporarily",
+        C: "Partially",
+        D: "Fully",
+        Answer: "A"
+    },
+    {
+        Question: "Which of the following is the highest speed slot?",
+        A: "ISA",
+        B: "AGP",
+        C: "PCI",
+        D: "AGC",
+        Answer: "B"
+    },
+    {
+        Question: "A video game is played by using        card",
+        A: "Sound",
+        B: "Modem",
+        C: "Graphics",
+        D: "Network information",
+        Answer: "C"
+    },
+    {
+        Question: "Barcode Reader is a/an           device",
+        A: "Output",
+        B: "Input",
+        C: "Display",
+        D: "Storage",
+        Answer: "B"
+    },
+    {
+        Question: "Which is used in security system and criminal investigations",
+        A: "Fingerprint Reader",
+        B: "Robots",
+        C: "Sound card",
+        D: "Barcode Reader",
+        Answer: "A"
+    },
+    {
+        Question: "Which is also known as mechanical agent",
+        A: "Computer",
+        B: "Barcode Reader",
+        C: "Processor",
+        D: "Robots",
+        Answer: "D"
+    },
+    {
+        Question: "_____ is the set of instruction given to the computer to perform a specific task",
+        A: "Monitor",
+        B: "Hardware",
+        C: "Software",
+        D: "Printer",
+        Answer: "C"
+    },
+    {
+        Question: "_____is the most famous type of Operating System for personal computer",
+        A: "Linux",
+        B: "Unix",
+        C: "Microsoft Windows",
+        D: "Mac OS",
+        Answer: "C"
+    },
+    {
+        Question: "A system software that helps Operating System to communicate with a device is called",
+        A: "Operating system",
+        B: "Device Driver",
+        C: "Utility",
+        D: "Graphics Software",
+        Answer: "B"
+    },
+    {
+        Question: "____ is a program that allows a user to analyze and maintain a computer",
+        A: "Utility",
+        B: "Device Driver",
+        C: "Windows XP",
+        D: "MS Office",
+        Answer: "A"
+    },
+    {
+        Question: "_____ creates a link between a user and the computer",
+        A: "Device Driver",
+        B: "Utilities",
+        C: "Operating System",
+        D: "Image Viewer",
+        Answer: "C"
+    },
+    {
+        Question: "A _________ keeps the mouse free from dust and dirt",
+        A: "Mouse pad",
+        B: "Rubber pad",
+        C: "Paper pad",
+        Answer: "A"
+    },
+    {
+        Question: "Keep the index finger on the ________ button",
+        A: "Left",
+        B: "Right",
+        C: "Middle",
+        Answer: "A"
+    },
+    {
+        Question: "Keep the middle finger on the ________ button",
+        A: "Right",
+        B: "Left",
+        C: "Scroll wheel",
+        Answer: "A"
+    },
+    {
+        Question: "Move the mouse over a ________",
+        A: "Mouse pad",
+        B: "Rubber pad",
+        C: "Surface",
+        Answer: "A"
+    },
+    {
+        Question: "Always keep the _________ on the mouse pad",
+        A: "Mouse",
+        B: "Mouse",
+        C: "Cursor",
+        Answer: "A"
+    },
+    {
+        Question: "_______ has a tail.",
+        A: "Mouse",
+        B: "CPU",
+        C: "Monitor",
+        Answer: "A"
+    },
+    {
+        Question: "A mouse is a part of a _________",
+        A: "Washing machine",
+        B: "Computer",
+        C: "Fan",
+        Answer: "B"
+    },
+    {
+        Question: "A keyboard has _________ keys.",
+        A: "Both",
+        B: "Number",
+        C: "Alphabet",
+        Answer: "A"
+    },
+    {
+        Question: "Cursor is a _________ blinking line on the monitor",
+        A: "Small",
+        B: "Big",
+        C: "Slanting",
+        Answer: "A"
+    },
+    {
+        Question: "There are usually __________ keys on the keyboard",
+        A: "104",
+        B: "106",
+        C: "110",
+        Answer: "A"
+    },
+    {
+        Question: "__________ keys are found at the top of the keyboard",
+        A: "Function",
+        B: "Number",
+        C: "Alphabet",
+        Answer: "A"
+    },
+    {
+        Question: "__________ key is use to write in capital letters",
+        A: "Caps Lock",
+        B: "Enter",
+        C: "Backspace",
+        Answer: "A"
+    },
+    {
+        Question: "________ erases letters to the left of the cursor",
+        A: "Backspace",
+        B: "Delete",
+        C: "Both",
+        Answer: "A"
+    },
+    {
+        Question: "Arrow keys are used to move the cursor in __________ directions",
+        A: "Both",
+        B: "LEFT/RIGHT",
+        C: "UP/DOWN",
+        Answer: "A"
+    },
+    {
+        Question: "Mouse is connected to _________",
+        A: "CPU",
+        B: "UPS",
+        C: "Keyboard",
+        Answer: "A"
+    },
+    {
+        Question: "Which Key Combination Is Used To Deselect A Selection",
+        A: "Ctrl + Enter",
+        B: "Ctrl + D",
+        C: "Ctrl + F",
+        Answer: "B"
+    },
+    {
+        Question: "Cable Television Is An Example Of……..",
+        A: "WAN",
+        B: "MAN",
+        C: "LAN",
+        Answer: "B"
+    },
+    {
+        Question: "This Refers To The Physical Layout Of The Network Computer And Links",
+        A: "Networking",
+        B: "Network Security",
+        C: "Topology",
+        Answer: "C"
+    },
+    {
+        Question: "It Is Not Important To Secure Wi Fi Network",
+        A: "True",
+        B: "False",
+        C: "None Of These",
+        Answer: "B"
+    },
+    {
+        Question: "Wireless Network Are . …….. To Install And Maintain .",
+        A: "Infrared",
+        B: "Wireless",
+        C: "Cheaper",
+        Answer: "C"
+    },
+    {
+        Question: "There Are. …,… Different Ways To Crop An Image",
+        A: "3",
+        B: "4",
+        C: "5",
+        Answer: "A"
+    },
+    {
+        Question: "Magnetic And Polygon Are The Two Type Of. …….Tool.",
+        A: "Lasso",
+        B: "Switch",
+        C: "Colors",
+        Answer: "A"
+    },
+    {
+        Question: "Clone Tool. Is Similar To Paint Bucket Tool",
+        A: "True",
+        B: "False",
+        C: "None Of These",
+        Answer: "B"
+    },
+    {
+        Question: "There Are Four Type Of Network.",
+        A: "True",
+        B: "False",
+        C: "None Of These",
+        Answer: "B"
+    },
+    {
+        Question: "Networking Is Only Used For Sharing Resources",
+        A: "True",
+        B: "False",
+        C: "None Of These",
+        Answer: "B"
+    },
+    {
+        Question: "WAN. Stands For Wide Area Network",
+        A: "True",
+        B: "False",
+        C: "None Of These",
+        Answer: "A"
+    },
+    {
+        Question: "Bluetooth Was Designed For Simple Wireless Communication",
+        A: "True",
+        B: "False",
+        C: "None Of These",
+        Answer: "B"
+    },
+    {
+        Question: "Which Images Help To Monitor And Modify.",
+        A: "Tool Panel",
+        B: "Pallets",
+        C: "Menu Bar",
+        Answer: "B"
+    },
+    {
+        Question: "What Do You Do With Photoshop Except One",
+        A: "Correcting Color",
+        B: "Retouching Image",
+        C: "Make A Presentation",
+        Answer: "C"
+    },
+    {
+        Question: "What Is Done To Copy.",
+        A: "Clone",
+        B: "Healing",
+        C: "Layer",
+        Answer: "A"
+    },
+    {
+        Question: "Lasso Tool Is Used To Select Part Of A Layer For Editing",
+        A: "False",
+        B: "True",
+        C: "None Of These",
+        Answer: "B"
+    },
+    {
+        Question: "…….,.. Palette Stores And Display Each Action Performe(D). On An Image",
+        A: "Website",
+        B: "Clone",
+        C: "History",
+        Answer: "C"
+    },
+    {
+        Question: "There Are Two Different Color Boxes Available In The Tool Box",
+        A: "False",
+        B: "True",
+        C: "None Of These",
+        Answer: "B"
+    },
+    {
+        Question: "Hi(DD).En Have Small Black Triangle At Right Hand Corner",
+        A: "False",
+        B: "True",
+        C: "None Of These",
+        Answer: "B"
+    },
+    {
+        Question: "WiMAX Technology Is A ……… Communication Digital System",
+        A: "WIFI",
+        B: "Wireless",
+        C: "Cheaper",
+        Answer: "B"
+    },
+    {
+        Question: "In. …….. Topology Each Computer Is Connected To Two Neighbouring Computer To Form Closed Circuit.",
+        A: "Ring",
+        B: "Bus",
+        C: "Star",
+        Answer: "A"
+    },
+    {
+        Question: "Network Security Is The Authorization Of A(CC).Ess To Data In A Network",
+        A: "False",
+        B: "True",
+        C: "None Of These",
+        Answer: "A"
+    },
+    {
+        Question: "There Are Mainly ……. Types Of Topologies In LAN Network",
+        A: "Three",
+        B: "Node",
+        C: "Bus",
+        Answer: "A"
+    },
+    {
+        Question: "Broadband Is A High Speed Internet Access",
+        A: "False",
+        B: "True",
+        C: "None Of These",
+        Answer: "B"
+    },
+    {
+        Question: "A. ………. Network Cover Large Geographical Area",
+        A: "LAN",
+        B: "WAN",
+        C: "TAN",
+        Answer: "B"
+    },
+    {
+        Question: "It Is Difficult To Extend Wi Fi",
+        A: "True",
+        B: "False",
+        C: "None Of These",
+        Answer: "A"
+    },
+    {
+        Question: "A Network Extends Bigger Area Than A LAN",
+        A: "False",
+        B: "True",
+        C: "None Of These",
+        Answer: "B"
+    },
+    {
+        Question: "Phishing Can Be Used To Gain Unauthorized Access To A Computer System",
+        A: "True",
+        B: "False",
+        C: "None Of These",
+        Answer: "A"
+    },
+    {
+        Question: "This Is Also Known As Physical Address",
+        A: "MAC Address",
+        B: "IP Address",
+        C: "IPV6",
+        Answer: "A"
+    },
+    {
+        Question: "Threatening Mail Is A Kind Of Network Security Attack",
+        A: "True",
+        B: "False",
+        C: "None Of These",
+        Answer: "A"
+    },
+    {
+        Question: "What Is The Name Of A Communication Media Which Is Use In LAN",
+        A: "LAN",
+        B: "WAN",
+        C: "MAN",
+        Answer: "C"
+    },
+    {
+        Question: "Router Is Used To Connect One Network To Other Network",
+        A: "True",
+        B: "False",
+        C: "None Of These",
+        Answer: "A"
+    },
+    {
+        Question: "LAN Stands For Local And Nationwide",
+        A: "True",
+        B: "False",
+        C: "None Of These",
+        Answer: "B"
+    },
+    {
+        Question: "HTTP Stands For Hyper Text Transfer Protocol",
+        A: "False",
+        B: "True",
+        C: "None Of These",
+        Answer: "B"
+    },
+    {
+        Question: "Access Point Is Similar To Hub",
+        A: "True",
+        B: "False",
+        C: "None Of These",
+        Answer: "A"
+    },
+    {
+        Question: "Repeater Is Used To Extend The Length Of The Cable",
+        A: "True",
+        B: "False",
+        C: "None Of These",
+        Answer: "A"
+    },
+    {
+        Question: "MAN Covers The Largest Area",
+        A: "True",
+        B: "False",
+        C: "None Of These",
+        Answer: "B"
+    },
+    {
+        Question: "RJ 45 Stands For Registered Jack 45",
+        A: "False",
+        B: "True",
+        C: "None Of These",
+        Answer: "B"
+    },
+    {
+        Question: "Wi Fi Was First Created To Be Used In Hospitals",
+        A: "True",
+        B: "False",
+        C: "None Of These",
+        Answer: "B"
+    },
+    {
+        Question: "Every Packet Of Data Travel Through Different Path",
+        A: "False",
+        B: "True",
+        C: "None Of These",
+        Answer: "A"
+    }
+];
 
 module.exports = { question }
